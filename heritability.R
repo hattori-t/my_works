e772f2b41541abba9cd28e69974bab7353f85f59
hattori-t <hattori@ut-biomet.org>
2016-02-23 11:14:51 +0900

    MEX2013 いちおう調べ終わり

--- heritability.R
+++ heritability.R
@@ -4,41 +4,31 @@ setwd("C:/Users/Tomo/Dropbox/sorghum/heritability")
 require(MCMCglmm)
 require(rrBLUP)
 
-#data
-pheno <- read.csv("data/pheno_mex2013_ver0.3_g.csv",row.names=1)
+#data (2013:MEX)
+pheno <- read.csv("data/pheno_mex2013_ver0.3_g.csv",row.names=1)  #dim(1305,9)
 pheno <- scale(pheno)
-SNP <- read.csv("data/inbred_SNP_list_by_stacks_geno_150120_sel1_imputed_trim_score_CMS.csv", row.names = 1)
-colnames(SNP) <- gsub("_res","",colnames(SNP))
-colnames(SNP)=gsub("B31.","B31/",colnames(SNP))
-colnames(SNP)=gsub("B2.","B2/",colnames(SNP))
+geno <- read.csv("data/inbred_SNP_list_by_stacks_geno_150120_sel1_imputed_trim_score_CMS.csv",
+                  row.names = 1)   #dim(127587,1305)
+colnames(geno) <- gsub("_res","",colnames(geno))
+colnames(geno)=gsub("B31.","B31/",colnames(geno))
+colnames(geno)=gsub("B2.","B2/",colnames(geno))
 
-#NA omit
-target <- pheno[,7]
-selector <- !is.na(target)
-pheno <- pheno[selector,]
+#test.data
+trait <- pheno[,9]         #choose the trait [,1~9]
+selector <- !is.na(trait)
+test.data <- pheno[selector,]
+test.data <- transform(test.data, X=rownames(test.data))
 
 #Ainverse
-xmat <- t(as.matrix(SNP))
-ymat <- pheno[rownames(xmat), ]
-rownames(ymat) <- rownames(xmat)
-y <- ymat[,7]         ### choose
-selector <- !is.na(y)
-x <- xmat[selector, ]   #x is t(SNP) with no NA rows
-amat <- A.mat(x, shrink = T)
+xmat <- t(as.matrix(geno))
+#x <- xmat[selector,]
+amat <- A.mat(xmat, shrink = T)
 Ainv <- solve(amat)
 Ainv <- as(Ainv,"sparseMatrix")
 
-#test.data
-test.data <- transform(pheno, X=rownames(pheno))
-X <- test.data[selector,]
-X <- X$X
-
 ##prior
-#prior <- list(G=list(G1=list(V=1,n=3)),R=list(V=1,n=3))  # What's the best number n ?
-### In JBS poster I didn't set a prior.
+prior <- list(G=list(G1=list(V=1,n=3)),R=list(V=1,n=3))  
 
 ## MCMC ##
-model_ionome <- MCMCglmm(juicy~1,random=~X,ginverse=list(X=Ainv),data=test.data #,prior=prior
-)
+model_ionome <- MCMCglmm(log.leaf.culm.weight~1,random=~X,ginverse=list(X=Ainv),data=test.data,prior=prior)
 summary(model_ionome)
-
