54e4ba9e0df5dafcb78f0d78e4a63da4fbaf9d2b
hattori-t <hattori@ut-biomet.org>
2016-09-11 14:14:00 +0900

    h2計算　条件ごとに一括で全形質を算出

--- heritability.R
+++ heritability.R
@@ -7,7 +7,6 @@ require(rrBLUP)
 ### parameters ###
 data <- commandArgs(trailingOnly=T)[1]
 snpcall <- commandArgs(trailingOnly=T)[2]
-traitname <- commandArgs(trailingOnly=T)[3]
 
 ## data
 pheno <- read.csv(paste("data/",data,".csv",sep=""), row.names=1)
@@ -23,23 +22,32 @@ Ainv <- as(Ainv,"sparseMatrix")
 pheno <- scale(pheno)
 test.data <- transform(pheno, X=rownames(pheno))
 
+#phenotype and MCMC
+result <- matrix(NA, nr=ncol(pheno), nc=3)
+
+for(i in 1:ncol(pheno)){
+  traitname <- colnames(pheno)[i]
+  
 trait <- test.data[,traitname]
 phenotype <- matrix(trait,ncol = 1)
 X <- test.data$X
 rownames(phenotype) <- X
 
-## prior
+# prior
 prior <- list(G=list(G1=list(V=1,n=0.002)),R=list(V=1,n=0.002))
 
 ## MCMC ##
 model <- MCMCglmm(phenotype~1,random=~X,ginverse=list(X=Ainv),data=test.data,prior=prior)
-save(model,file=paste("heritability_",data,"_",snpcall,"_",traitname,".data",sep=""))
 
 res <- matrix(NA,nr=1,nc=3)
-rownames(res) <- traitname
-colnames(res) <- c("h2","95%CI-","-95%CI")
 cor <- model$VCV[,1]/(model$VCV[,1] + model$VCV[,2])
 res[,1] <- mean(cor)
 res[,2] <- HPDinterval(cor)[1]
 res[,3] <- HPDinterval(cor)[2]
-write.csv(res,paste("heritability_",data,"_",snpcall,"_",traitname,".csv",sep=""))
+  
+  result[i,] <- res
+}
+
+rownames(result) <- colnames(pheno)
+colnames(result) <- c("h2","95%CI-","-95%CI")
+write.csv(result,paste("heritability_",data,"_",snpcall,".csv",sep=""))
