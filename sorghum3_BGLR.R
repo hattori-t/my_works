5953d65875636ceae80a6d0989616ddf05c293ba
Tomohiro Hattori <tomo@Tomos-PC.local>
2017-02-12 02:49:26 +0900

    gauss LL推定を100通りに変更

--- sorghum3_BGLR.R
+++ sorghum3_BGLR.R
@@ -82,7 +82,7 @@ for(k in 1:ncol(pheno)){
     testname_B31 <- paste("B31/",testname,sep = "")
     removes <- c(testname,testname_B2,testname_B31)
     usingname <- name[!(name %in% removes)]
-    
+
     y <- pheno[usingname,traitname]
     y.train <- c(y, NA)
     xname <- c(usingname,rownames(test)[i])
@@ -123,13 +123,13 @@ colnames(result) <- c("r","rmse")
 for(k in 1:ncol(pheno)){
   traitname <- colnames(pheno)[k]
   print(traitname)
-  
+
   y <- pheno[, traitname]
-  
+
   # remove missing samples
   selector <- !is.na(y)
   name <- rownames(pheno)[selector]
-  
+
   # predict
   y.pred <- rep(NA, length(rownames(test)))
   for(i in 1:nrow(test)) {
@@ -139,7 +139,7 @@ for(k in 1:ncol(pheno)){
     testname_B31 <- paste("B31/",testname,sep = "")
     removes <- c(testname,testname_B2,testname_B31)
     usingname <- name[!(name %in% removes)]
-    
+
     y <- pheno[usingname,traitname]
     y.train <- c(y, NA)
     xname <- c(usingname,rownames(test)[i])
@@ -149,9 +149,9 @@ for(k in 1:ncol(pheno)){
     res <- BGLR(y = y.train, ETA = ETA, verbose = F)
     y.pred[i] <- tail(res$yHat,1)
   }
-  
+
   prediction[,k] <- as.vector(y.pred)
-  
+
   #plot
   pdf(paste("LOO_",data,"_",trainingdata,"_to_",testdata,"/",regression,"/",traitname,".pdf",sep = ""))
   plot(test[,k], prediction[,k], xlab = "Observed Value", ylab = "Predicted Value", main = paste(colnames(test)[k],"_",regression,sep = ""))
@@ -161,7 +161,7 @@ for(k in 1:ncol(pheno)){
   rmse <- sqrt(mse)
   legend("bottomright", legend = paste("r=", round(cor,2), " rmse=", round(rmse,2), sep = ""), bty="n")
   dev.off()
-  
+
   result[k,] <- c(cor,rmse)
 }
 
@@ -181,28 +181,28 @@ colnames(result) <- c("r","rmse")
 res_theta <- matrix(NA, nrow = 11, ncol = 1)
 rownames(res_theta) <- colnames(pheno)
 colnames(res_theta) <- c("theta")
-para <- c(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0)
+para <- c(1:100)/10
 
 for(k in 1:ncol(pheno)){
   traitname <- colnames(pheno)[k]
   print(traitname)
-  
+
   y <- pheno[, traitname]
-  
+
   # remove missing samples
   selector <- !is.na(y)
   name <- rownames(pheno)[selector]
-  
+
   # estimate LL and decide theta
   require(rrBLUP)
-  LL <- rep(NA,10)
-  for(Z in 1:10){
+  LL <- rep(NA,length(para))
+  for(Z in 1:length(para)){
     est <- mixed.solve(y[selector], K = exp(-(d[name,name]/para[Z])^2))
     LL[Z] <- est$LL
   }
   para_number <- which(LL >= max(LL))
   theta <- para[para_number]
-  
+
   # predict
   y.pred <- rep(NA, length(rownames(test)))
   for(i in 1:nrow(test)) {
@@ -212,7 +212,7 @@ for(k in 1:ncol(pheno)){
     testname_B31 <- paste("B31/",testname,sep = "")
     removes <- c(testname,testname_B2,testname_B31)
     usingname <- name[!(name %in% removes)]
-    
+
     y <- pheno[usingname,traitname]
     y.train <- c(y, NA)
     xname <- c(usingname,rownames(test)[i])
@@ -221,9 +221,9 @@ for(k in 1:ncol(pheno)){
     res <- BGLR(y = y.train, ETA = ETA, verbose = F)
     y.pred[i] <- tail(res$yHat,1)
   }
-  
+
   prediction[,k] <- as.vector(y.pred)
-  
+
   #plot
   pdf(paste("LOO_",data,"_",trainingdata,"_to_",testdata,"/",regression,"/",traitname,".pdf",sep = ""))
   plot(test[,k], prediction[,k], xlab = "Observed Value", ylab = "Predicted Value", main = paste(colnames(test)[k],"_",regression,sep = ""))
@@ -233,7 +233,7 @@ for(k in 1:ncol(pheno)){
   rmse <- sqrt(mse)
   legend("bottomright", legend = paste("r=", round(cor,2), " rmse=", round(rmse,2), sep = ""), bty="n")
   dev.off()
-  
+
   result[k,] <- c(cor,rmse)
   res_theta[k,] <- theta
 }
