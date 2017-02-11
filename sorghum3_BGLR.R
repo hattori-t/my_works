e6171db0eed53b9bf167da9aa08763c175a8e70f
Tomohiro Hattori <tomo@Tomos-PC.local>
2017-02-11 11:35:40 +0900

    gaussian追加

--- sorghum3_BGLR.R
+++ sorghum3_BGLR.R
@@ -167,3 +167,77 @@ for(k in 1:ncol(pheno)){
 
 write.csv(prediction, paste("LOO_",data,"_",trainingdata,"_to_",testdata,"/",regression,"/predictedvalues_",regression,".csv",sep = ""))
 write.csv(result, paste("LOO_",data,"_",trainingdata,"_to_",testdata,"/",regression,"/result_",regression,".csv",sep = ""))
+
+
+## Gausian kernel ##
+regression <- "GAUSS"
+dir.create(paste("LOO_",data,"_",trainingdata,"_to_",testdata,"/",regression, sep = ""))
+
+prediction <- matrix(NA, nr=nrow(test), nc=ncol(test), dimnames=dimnames(test))
+result <- matrix(NA, nrow = 11, ncol = 2)
+rownames(result) <- colnames(pheno)
+colnames(result) <- c("r","rmse")
+
+res_theta <- matrix(NA, nrow = 11, ncol = 1)
+rownames(res_theta) <- colnames(pheno)
+colnames(res_theta) <- c("theta")
+para <- c(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0)
+
+for(k in 1:ncol(pheno)){
+  traitname <- colnames(pheno)[k]
+  print(traitname)
+  
+  y <- pheno[, traitname]
+  
+  # remove missing samples
+  selector <- !is.na(y)
+  name <- rownames(pheno)[selector]
+  
+  # estimate LL and decide theta
+  require(rrBLUP)
+  LL <- rep(NA,10)
+  for(Z in 1:10){
+    est <- mixed.solve(y[selector], K = exp(-(d[name,name]/para[Z])^2))
+    LL[Z] <- est$LL
+  }
+  para_number <- which(LL >= max(LL))
+  theta <- para[para_number]
+  
+  # predict
+  y.pred <- rep(NA, length(rownames(test)))
+  for(i in 1:nrow(test)) {
+    print(paste(i,"/",nrow(test),sep=""))
+    testname <- gsub("B[[:digit:]]/","",rownames(test)[i])
+    testname_B2 <- paste("B2/",testname,sep = "")
+    testname_B31 <- paste("B31/",testname,sep = "")
+    removes <- c(testname,testname_B2,testname_B31)
+    usingname <- name[!(name %in% removes)]
+    
+    y <- pheno[usingname,traitname]
+    y.train <- c(y, NA)
+    xname <- c(usingname,rownames(test)[i])
+    x <- d[xname,xname]
+    ETA <- list(list(K = exp(-(x/theta)^2), model = "RKHS"))
+    res <- BGLR(y = y.train, ETA = ETA, verbose = F)
+    y.pred[i] <- tail(res$yHat,1)
+  }
+  
+  prediction[,k] <- as.vector(y.pred)
+  
+  #plot
+  pdf(paste("LOO_",data,"_",trainingdata,"_to_",testdata,"/",regression,"/",traitname,".pdf",sep = ""))
+  plot(test[,k], prediction[,k], xlab = "Observed Value", ylab = "Predicted Value", main = paste(colnames(test)[k],"_",regression,sep = ""))
+  abline(0, 1, lty = "dotted")
+  cor <- cor(test[,k], prediction[,k], use="pair")
+  mse <- sum((test[,k] - prediction[,k])^2,na.rm = T) / length(test[,k])
+  rmse <- sqrt(mse)
+  legend("bottomright", legend = paste("r=", round(cor,2), " rmse=", round(rmse,2), sep = ""), bty="n")
+  dev.off()
+  
+  result[k,] <- c(cor,rmse)
+  res_theta[k,] <- theta
+}
+
+write.csv(prediction, paste("LOO_",data,"_",trainingdata,"_to_",testdata,"/",regression,"/predictedvalues_",regression,".csv",sep = ""))
+write.csv(result, paste("LOO_",data,"_",trainingdata,"_to_",testdata,"/",regression,"/result_",regression,".csv",sep = ""))
+write.csv(res_theta, paste("LOO_",data,"_",trainingdata,"_to_",testdata,"/",regression,"/res_theta_",regression,".csv",sep = ""))
