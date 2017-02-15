1835b5cf7e1e83d0138cdbd3873cd6adb400aecf
hattori-t <hattori@ut-biomet.org>
2017-02-15 20:15:53 +0900

    kin.blup debug

--- sorghum3_rrBLUP_ver2.R
+++ sorghum3_rrBLUP_ver2.R
@@ -1,6 +1,6 @@
 setwd("/Users/tomo/Dropbox/sorghum3")
 
-####### LOO for 3 training data <rrBLUP> : inbred, F1-x, inbred+F1-x (different from testdata)
+####### LOO for 4 training data <rrBLUP> : F1-x, inbred+F1-x, F1-A+B, all
 
 ### parameters ###
 data <- commandArgs(trailingOnly = T)[1]
@@ -46,12 +46,12 @@ test <- test[selecting_test,]
 
 test <- test[!(rownames(test) %in% c("B2", "B31")),]
 
-dir.create(paste("rrBLUP_LOO_",data,"_",trainingdata,"_to_",testdata, sep = ""))
+dir.create(paste("rrBULP_LOO_",data,"_",trainingdata,"_to_",testdata, sep = ""))
 
 
 ## G-BLUP ##
 regression <- "G-BLUP"
-dir.create(paste("rrBLUP_LOO_",data,"_",trainingdata,"_to_",testdata,"/",regression, sep = ""))
+dir.create(paste("rrBULP_LOO_",data,"_",trainingdata,"_to_",testdata,"/",regression, sep = ""))
 
 prediction <- matrix(NA, nr=nrow(test), nc=ncol(test), dimnames=dimnames(test))
 result <- matrix(NA, nrow = 11, ncol = 2)
@@ -62,9 +62,8 @@ for(k in 1:ncol(pheno)){
   traitname <- colnames(pheno)[k]
   print(traitname)
 
-  y <- pheno[, traitname]
-
   # remove missing samples
+  y <- pheno[, traitname]
   selector <- !is.na(y)
   name <- rownames(pheno)[selector]
 
@@ -72,25 +71,26 @@ for(k in 1:ncol(pheno)){
   y.pred <- rep(NA, length(rownames(test)))
   for(i in 1:nrow(test)) {
     print(paste(i,"/",nrow(test),sep=""))
-    testname <- gsub("B[[:digit:]]/","",rownames(test)[i])
-    testname_B2 <- paste("B2/",testname,sep = "")
-    testname_B31 <- paste("B31/",testname,sep = "")
-    removes <- c(testname,testname_B2,testname_B31)
-    usingname <- name[!(name %in% removes)]
+    testname <- rownames(test)[i]
+    name_inbred <- gsub("B[[:digit:]]/","",testname)
+    name_B2 <- paste("B2/",name_inbred,sep = "")
+    name_B31 <- paste("B31/",name_inbred,sep = "")
+    remove_names <- c(name_inbred,name_B2,name_B31)
+    removes <- intersect(remove_names, rownames(pheno))
 
-    y <- pheno[usingname,traitname]
-    y.train <- c(y, NA)
-    xname <- c(usingname,rownames(test)[i])
-    x <- amat[xname,xname]
-    input <- data.frame(y=y.train,ID=xname)
+    y <- pheno
+    y[removes,traitname] <- NA
+    y.train <- y[,traitname]
+    x <- amat[rownames(y),rownames(y)]
+    input <- data.frame(y=y.train,ID=rownames(y))
     res <- kin.blup(input, geno="ID", pheno="y", K=x)
-    y.pred[i] <- tail(res$pred,1)
+    y.pred[i] <- res$pred[which(rownames(y)==testname)]
   }
 
   prediction[,k] <- as.vector(y.pred)
 
   #plot
-  pdf(paste("rrBLUP_LOO_",data,"_",trainingdata,"_to_",testdata,"/",regression,"/",traitname,".pdf",sep = ""))
+  pdf(paste("rrBULP_LOO_",data,"_",trainingdata,"_to_",testdata,"/",regression,"/",traitname,".pdf",sep = ""))
   plot(test[,k], prediction[,k], xlab = "Observed Value", ylab = "Predicted Value", main = paste(colnames(test)[k],"_",regression,sep = ""))
   abline(0, 1, lty = "dotted")
   cor <- cor(test[,k], prediction[,k], use="pair")
@@ -102,14 +102,13 @@ for(k in 1:ncol(pheno)){
   result[k,] <- c(cor,rmse)
 }
 
-write.csv(prediction, paste("rrBLUP_LOO_",data,"_",trainingdata,"_to_",testdata,"/",regression,"/predictedvalues_",regression,".csv",sep = ""))
-write.csv(result, paste("rrBLUP_LOO_",data,"_",trainingdata,"_to_",testdata,"/",regression,"/result_",regression,".csv",sep = ""))
-
+write.csv(prediction, paste("rrBULP_LOO_",data,"_",trainingdata,"_to_",testdata,"/",regression,"/predictedvalues_",regression,".csv",sep = ""))
+write.csv(result, paste("rrBULP_LOO_",data,"_",trainingdata,"_to_",testdata,"/",regression,"/result_",regression,".csv",sep = ""))
 
 
 ## Gausian kernel ##
 regression <- "GAUSS"
-dir.create(paste("rrBLUP_LOO_",data,"_",trainingdata,"_to_",testdata,"/",regression, sep = ""))
+dir.create(paste("rrBULP_LOO_",data,"_",trainingdata,"_to_",testdata,"/",regression, sep = ""))
 
 prediction <- matrix(NA, nr=nrow(test), nc=ncol(test), dimnames=dimnames(test))
 result <- matrix(NA, nrow = 11, ncol = 2)
@@ -120,9 +119,8 @@ for(k in 1:ncol(pheno)){
   traitname <- colnames(pheno)[k]
   print(traitname)
 
-  y <- pheno[, traitname]
-
   # remove missing samples
+  y <- pheno[, traitname]
   selector <- !is.na(y)
   name <- rownames(pheno)[selector]
 
@@ -130,25 +128,26 @@ for(k in 1:ncol(pheno)){
   y.pred <- rep(NA, length(rownames(test)))
   for(i in 1:nrow(test)) {
     print(paste(i,"/",nrow(test),sep=""))
-    testname <- gsub("B[[:digit:]]/","",rownames(test)[i])
-    testname_B2 <- paste("B2/",testname,sep = "")
-    testname_B31 <- paste("B31/",testname,sep = "")
-    removes <- c(testname,testname_B2,testname_B31)
-    usingname <- name[!(name %in% removes)]
+    testname <- rownames(test)[i]
+    name_inbred <- gsub("B[[:digit:]]/","",testname)
+    name_B2 <- paste("B2/",name_inbred,sep = "")
+    name_B31 <- paste("B31/",name_inbred,sep = "")
+    remove_names <- c(name_inbred,name_B2,name_B31)
+    removes <- intersect(remove_names, rownames(pheno))
 
-    y <- pheno[usingname,traitname]
-    y.train <- c(y, NA)
-    xname <- c(usingname,rownames(test)[i])
-    x <- d[xname,xname]
-    input <- data.frame(y=y.train,ID=xname)
+    y <- pheno
+    y[removes,traitname] <- NA
+    y.train <- y[,traitname]
+    x <- d[rownames(y),rownames(y)]
+    input <- data.frame(y=y.train,ID=rownames(y))
     res <- kin.blup(input, geno="ID", pheno="y", K=x, GAUSS=T)
-    y.pred[i] <- tail(res$pred,1)
+    y.pred[i] <- res$pred[which(rownames(y)==testname)]
   }
 
   prediction[,k] <- as.vector(y.pred)
 
   #plot
-  pdf(paste("rrBLUP_LOO_",data,"_",trainingdata,"_to_",testdata,"/",regression,"/",traitname,".pdf",sep = ""))
+  pdf(paste("rrBULP_LOO_",data,"_",trainingdata,"_to_",testdata,"/",regression,"/",traitname,".pdf",sep = ""))
   plot(test[,k], prediction[,k], xlab = "Observed Value", ylab = "Predicted Value", main = paste(colnames(test)[k],"_",regression,sep = ""))
   abline(0, 1, lty = "dotted")
   cor <- cor(test[,k], prediction[,k], use="pair")
@@ -160,5 +159,5 @@ for(k in 1:ncol(pheno)){
   result[k,] <- c(cor,rmse)
 }
 
-write.csv(prediction, paste("rrBLUP_LOO_",data,"_",trainingdata,"_to_",testdata,"/",regression,"/predictedvalues_",regression,".csv",sep = ""))
-write.csv(result, paste("rrBLUP_LOO_",data,"_",trainingdata,"_to_",testdata,"/",regression,"/result_",regression,".csv",sep = ""))
+write.csv(prediction, paste("rrBULP_LOO_",data,"_",trainingdata,"_to_",testdata,"/",regression,"/predictedvalues_",regression,".csv",sep = ""))
+write.csv(result, paste("rrBULP_LOO_",data,"_",trainingdata,"_to_",testdata,"/",regression,"/result_",regression,".csv",sep = ""))
